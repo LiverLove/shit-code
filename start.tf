727e64dfbb22a12f11becbdfe6463f655afc10ec
Ivan Terichev <teriv@list.ru>
2019-05-31 01:52:01 +0800

    update

--- start.tf
+++ start.tf
@@ -1,19 +1,28 @@
 provider "aws" {
-   access_key	= "${var.access_key}"
-   secret_key	= "${var.secret_key}"
-   region	= "us-east-2"
+   access_key		= "${var.access_key}"
+   secret_key		= "${var.secret_key}"
+   region		= "us-east-2"
    shared_credentials_file = "/home/akakiy/.ssh"
-   profile	= "ld_rsa"  
+   profile		= "ld_rsa"  
 }
 
 resource "aws_instance" "web" {
-   ami	= "ami-0ebbf2179e615c338"
-   instance_type = "t2.micro"
-   
-   key_name = "liver"
-   subnet_id = "${aws_subnet.PN1.id}"
+   ami			= "ami-0ebbf2179e615c338"
+   instance_type 	= "t2.micro"
+   security_groups 	= ["${aws_security_group.allow_ssh.id}"] 
+   key_name 		= "liver"
+   subnet_id 		= "${aws_subnet.PN1.id}"
+   associate_public_ip_address = true
+}
+
+resource "aws_instance" "back" {
+   ami			= "ami-0ebbf2179e615c338"
+   instance_type 	= "t2.micro"
+   security_groups 	= ["${aws_security_group.allow_ssh.id}"]
+   key_name		= "liver"
+   subnet_id		= "${aws_subnet.PN2.id}"
 }
 
 resource "aws_eip" "bastion" {
-   vpc = true
+   vpc 			= true
 }
--- vpc.tf
+++ vpc.tf
@@ -8,41 +8,41 @@ resource "aws_vpc" "bastion" {
 }
 
 resource "aws_subnet" "PN1" {
-   vpc_id	= "${aws_vpc.bastion.id}"
-   cidr_block	= "10.0.1.0/24"
-   availability_zone = "us-east-2a"
+   vpc_id		= "${aws_vpc.bastion.id}"
+   cidr_block		= "10.0.1.0/24"
+   availability_zone 	= "us-east-2a"
    map_public_ip_on_launch = "true"
 
    tags = {
-      Name = "PN1"
+      Name 		= "PN1"
    }
 }
 
 resource "aws_subnet" "PN2" {
-   vpc_id	= "${aws_vpc.bastion.id}"
-   cidr_block	= "10.0.2.0/24"
-   availability_zone = "us-east-2b"
+   vpc_id		= "${aws_vpc.bastion.id}"
+   cidr_block		= "10.0.2.0/24"
+   availability_zone 	= "us-east-2b"
 
    tags = {
-      Name = "PN2"
+      Name 		= "PN2"
    }
 }
 
 resource "aws_subnet" "PN3" {
-   vpc_id	= "${aws_vpc.bastion.id}"
-   cidr_block	= "10.0.3.0/24"
-   availability_zone = "us-east-2c"
+   vpc_id		= "${aws_vpc.bastion.id}"
+   cidr_block		= "10.0.3.0/24"
+   availability_zone 	= "us-east-2c"
 
    tags = {
-      Name = "PN3"
+      Name 		= "PN3"
    }
 }
 
 resource "aws_internet_gateway" "bastion" {
-   vpc_id 	= "${aws_vpc.bastion.id}"
+   vpc_id 		= "${aws_vpc.bastion.id}"
 
    tags = {
-      Name = "${var.project_name}"
+      Name 		= "${var.project_name}"
    }
 }
 
@@ -51,46 +51,20 @@ resource "aws_nat_gateway" "bastion" {
    subnet_id		= "${aws_subnet.PN1.id}"
 
    tags = {
-      Name = "gw NAT"
+      Name 		= "gw NAT"
    }
 }
 
 resource "aws_security_group" "allow_ssh" {
-   name		= "allow_ssh"
-   description	= "Allow SSH inbound traffic"
-   vpc_id	= "${aws_vpc.bastion.id}"
+   name			= "allow_ssh"
+   description		= "Allow SSH inbound traffic"
+   vpc_id		= "${aws_vpc.bastion.id}"
 
    ingress {
-      from_port	= 22
-      to_port	= 22
-      protocol	= "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-   }
-
-   tags = {
-      Name = "${var.project_name}"
-   }
-}
-
-resource "aws_route_table" "pub" {
-   vpc_id	= "${aws_vpc.bastion.id}"
- 
-   route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = "${aws_internet_gateway.bastion.id}"
-   }
-
-   tags = {
-      Name 	= "${var.project_name}_pub"
-   }
-}
-
-resource "aws_route_table" "priv" {
-   vpc_id	= "${aws_vpc.bastion.id}"
-   
-   route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = "${aws_nat_gateway.bastion.id}"
+      from_port		= 0
+      to_port		= 22
+      protocol		= "tcp"
+      cidr_blocks	= ["0.0.0.0/0"]
    }
 
    tags = {
@@ -98,7 +72,38 @@ resource "aws_route_table" "priv" {
    }
 }
 
+resource "aws_route_table" "pub" {
+   vpc_id		= "${aws_vpc.bastion.id}"
+ 
+   route {
+      cidr_block 	= "0.0.0.0/0"
+      gateway_id 	= "${aws_internet_gateway.bastion.id}"
+   }
+
+   tags = {
+      Name 		= "${var.project_name}_pub"
+   }
+}
+
+resource "aws_route_table" "priv" {
+   vpc_id		= "${aws_vpc.bastion.id}"
+   
+   route {
+      cidr_block 	= "0.0.0.0/0"
+      gateway_id 	= "${aws_nat_gateway.bastion.id}"
+   }
+
+   tags = {
+      Name 		= "${var.project_name}"
+   }
+}
+
 resource "aws_route_table_association" "pub_ass" {
-   subnet_id	= "${aws_subnet.PN1.id}"
-   route_table_id = "${aws_route_table.pub.id}"
+   subnet_id		= "${aws_subnet.PN1.id}"
+   route_table_id 	= "${aws_route_table.pub.id}"
+}
+
+resource "aws_route_table_association" "priv_ass" {
+   subnet_id		= "${aws_subnet.PN2.id}"
+   route_table_id	= "${aws_route_table.priv.id}"
 }
